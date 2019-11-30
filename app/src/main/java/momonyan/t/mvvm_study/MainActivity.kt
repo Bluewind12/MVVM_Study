63ddaa806cc45e72e12e66be27ab95314fc35828
“BlueWind_Mac” <zero18030@gmail.com>
2019-11-30 19:02:43 +0900

    [update]LiveData入れる。 オブザーバーすごい！

--- app/src/main/java/momonyan/t/mvvm_study/MainActivity.kt
+++ app/src/main/java/momonyan/t/mvvm_study/MainActivity.kt
@@ -2,6 +2,8 @@ package momonyan.t.mvvm_study
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 import momonyan.t.mvvm_study.ViewModel.ContentViewModel
@@ -13,16 +15,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //ViewModel
         val viewModel = ViewModelProviders.of(this).get(ContentViewModel::class.java)
-
+//        val viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(application).create(ContentViewModel::class.java)
         //比較用
         var count: Int = 0
 
         button.setOnClickListener {
             count++
             viewModel.viewModelCount++
+            viewModel.viewModelLiveDataCount.value = viewModel.viewModelLiveDataCount.value!! + 1
 
             textView.text = count.toString()
             textView2.text = viewModel.viewModelCount.toString()
+            textView3.text = viewModel.viewModelLiveDataCount.value.toString()
         }
+
+        //オブザーバー
+        val countObserver = Observer<Int> {
+            textView3.text = viewModel.viewModelLiveDataCount.value.toString()
+        }
+
+        //オブザーバーセット。
+        viewModel.viewModelLiveDataCount.observe(this, countObserver)
     }
 }
--- app/src/main/java/momonyan/t/mvvm_study/ViewModel/ContentViewModel.kt
+++ app/src/main/java/momonyan/t/mvvm_study/ViewModel/ContentViewModel.kt
@@ -1,7 +1,15 @@
 package momonyan.t.mvvm_study.ViewModel
 
+import android.view.LayoutInflater
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+
 class ContentViewModel : ViewModel() {
+
     var viewModelCount: Int = 0
+
+    private val _viewModelLiveDataCount = MutableLiveData(0)
+    var viewModelLiveDataCount = _viewModelLiveDataCount
+
 }
